555b8ddab187a76c366a096a2769951566cb482a
Rayan <rayankasam12@gmail.com>
2023-06-01 21:42:26 -0400

    Added function to check a users' login information

---
+++ checkPass.py
@@ -0,0 +1,45 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from datetime import datetime
+from argon2 import PasswordHasher
+
+
+load_dotenv(Path('./database.env'))
+DB_IP = os.getenv('IP')
+DB_PORT = os.getenv('PORT')
+DB_PASSWORD = os.getenv('PASSWORD')
+DB_USER = os.getenv('USERNAME')
+
+
+# Establishing databse connection
+db = mysql.connector.connect(
+host=DB_IP,
+port=DB_PORT,
+user=DB_USER,
+database="WFM_MAIN_INFO",
+password=DB_PASSWORD
+)
+mycursor = db.cursor()
+
+def checkPassword(email,password):
+    mycursor.execute("SELECT password FROM user WHERE email = %s",(email,))
+    print("Query Executed")
+    hashedPass = mycursor.fetchone()[0]
+    print(f"password is {hashedPass}")
+    ph = PasswordHasher()
+    passwordMatch = False
+    try:
+        passwordMatch = ph.verify(hashedPass,password)
+    except:
+        ...
+    return passwordMatch
+def getLogin():
+    email = input("Enter E-mail: ")
+    password = input("Enter Password: ")
+    return email, password
+email, password = getLogin()
+print(checkPassword(email,password))
+
+
--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, render_template_string,url_for, session, request
 app = Flask(__name__)
+app.secret_key = b"2234JHG3[]opuhmiy757n7ijNT756654"
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template_string("""
+            {% if session['username'] %}
+                <h1> Logged in as {{session['username']}}</h1>
+            {% else %}
+                <h1>Welcome! Please enter your email <a href="{{ url_for('login') }}">here.</a></h1>
 
-@app.route('/other')
-def other():
-    return "You've made it to other"
+            {% endif %}
+                """)    
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['password']
+            session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <h3>Username<\h3>
+            <p><input type=text name=username>
+            <h3>Password<\h3>
+            <p><input type=text name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+@app.route('/addEmployee',methods=['GET','POST'])
+def addEmployee():
+    if request.method == 'POST':
+        ...
+    return '''
+        <>
+    '''
+if __name__ == '__main__':
+    app.run()
