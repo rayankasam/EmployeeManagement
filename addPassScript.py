b5f8a11ae81b1e70af6fbbf17f091813362b3cf6
Rayan <rayankasam12@gmail.com>
2023-06-02 21:17:30 -0400

    working on adding passwords for all users

--- addPassScript.py
+++ addPassScript.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import os
+import string
+import random
 from dotenv import load_dotenv
 from pathlib import Path
 from datetime import datetime
@@ -21,7 +23,30 @@ user=DB_USER,
 database="WFM_MAIN_INFO",
 password=DB_PASSWORD
 )
+def generatePass():
+    password = ""
+    chars = string.ascii_letters() + string.digits()
+    for i in range(random.randint(7,12)):
+        password = password + chars[random.randint(len(chars))-1]
+    return password
 
 mycursor = db.cursor()
 
-mycursor.execute("SELECT ")
+mycursor.execute("SELECT email, employeeID FROM employeeInfo")
+data = mycursor.fetchall()
+
+for i in data:
+    employeeID = i[1]
+    email = i[0]
+    mycursor.execute("SELECT COUNT(1) FROM user WHERE employeeID = %s",(employeeID,))
+    
+    if mycursor.fetchone()[0] == 0:
+
+#    if mycursor.fetchone()[0] == 0:
+#        #Add to user
+#        mycursor.execute("INSERT INTO user (email,password,employeeID) VALUES (%s,%s,%s)",(email,password,employeeID))
+
+mycursor.close()
+db.close()
+
+
