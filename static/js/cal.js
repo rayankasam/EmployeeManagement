cb03c0b3cecf3c983ffd8ddc87e7910f875d114f
Karamvir <karam.hira10@gmail.com>
2023-06-30 14:13:29 -0400

    Calendar update

--- static/js/cal.js
+++ static/js/cal.js
@@ -4,10 +4,10 @@ import timeGridPlugin   from '@fullcalendar/timegrid';
 import listPlugin  from '@fullcalendar/list';
 
 
-
     let calendarEl = document.getElementById('calendar');
     
     let calendar = new Calendar(calendarEl,{
+      aspectRatio: 2,
       plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
       initialView: 'dayGridMonth',
       headerToolbar: {
