4e175fe674436e022f880303516cb80b1036b17f
Karamvir <karam.hira10@gmail.com>
2023-06-30 14:04:25 -0400

    added more veiws to the calender

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: ['./static/js/cal.mjs', './node_modules/@fullcalendar/core/index.global.min.js'],
+  entry: ['./static/js/cal.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'static', 'dist'),
