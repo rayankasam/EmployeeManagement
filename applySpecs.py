3d1a359c725dd7e76d08109c56798f56994872d8
Rayan <rayankasam12@gmail.com>
2024-01-13 11:54:45 -0500

    Updated requirements.txt

--- applySpecs.py
+++ applySpecs.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 load_dotenv(Path('./defaults.env'))
-MAX_PERM_ADDEMPLOYEE= int(os.getenv('MAX_PERM_ADDEMPLOYEE'))
+MAX_PERM_ADDEMPLOYEE = int(os.getenv('MAX_PERM_ADDEMPLOYEE'))
 
 
 def allowedAddEmployee(permission):
     return permission <= MAX_PERM_ADDEMPLOYEE
-
